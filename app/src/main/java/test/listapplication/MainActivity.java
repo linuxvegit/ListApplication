4b7a139f44b008e4c3dac29c0decc6174e16d7b2
linuxvegit <linuxve@126.com>
2015-01-09 21:10:03 +0800

    add photo

--- app/src/main/java/test/listapplication/MainActivity.java
+++ app/src/main/java/test/listapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package test.listapplication;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -12,6 +13,7 @@ import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.text.style.UnderlineSpan;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -23,6 +25,9 @@ import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,6 +42,8 @@ import java.util.Objects;
  * Created by MouXK on 15/1/6.
  */
 public class MainActivity extends Activity {
+    private final static String TAG = "MainActivity";
+
     private RefreshableView refreshableView;
     private ListView listView;
 
@@ -107,6 +114,15 @@ public class MainActivity extends Activity {
             map.put("detail", details[i]);
             map.put("content", contents[i]);
             map.put("isLike", false);
+            if (i == 0) {
+                ArrayList<Integer> photoList = new ArrayList<>();
+                photoList.add(R.drawable.icon1);
+                photoList.add(R.drawable.icon);
+                photoList.add(R.drawable.icon2);
+                photoList.add(R.drawable.icon3);
+                map.put("photos", photoList);
+            } else
+                map.put("photos", null);
             list.add(map);
         }
     }
@@ -137,12 +153,11 @@ public class MainActivity extends Activity {
         public TextView detailMsg;//状态附属属性
         public TextView content;//状态具体内容
         public TextView updateTime;//状态发送时间
+        public TableLayout photoLayout;//照片显示布局
         public ImageButton goodButton;//点赞按钮
     }
 
     public class MyAdapter extends BaseAdapter {
-        private boolean isTitleHolder = false;
-
         private LayoutInflater inflater;
 
         public MyAdapter(Context context) {
@@ -166,7 +181,9 @@ public class MainActivity extends Activity {
             if (position == 0) {
                 TitleHolder holder = null;
 
-                if (convertView == null || !isTitleHolder) {
+                if (convertView != null && convertView.getTag() instanceof TitleHolder) {
+                    holder = (TitleHolder) convertView.getTag();
+                } else {
                     holder = new TitleHolder();
                     //初始化标题元素
                     convertView = inflater.inflate(R.layout.title, null);
@@ -174,10 +191,8 @@ public class MainActivity extends Activity {
                     holder.iconImg = (ImageView) convertView.findViewById(R.id.titleIcon);
                     holder.userName = (TextView) convertView.findViewById(R.id.titleUserName);
                     convertView.setTag(holder);
-                } else {
-                    holder = (TitleHolder) convertView.getTag();
                 }
-                isTitleHolder = true;
+
                 //设置用户名
                 holder.userName.setText(getResources().getString((Integer) titleMap.get("userName")));
                 //设置用户头像
@@ -197,7 +212,9 @@ public class MainActivity extends Activity {
             else {
                 MsgHolder holder = null;
 
-                if (convertView == null || isTitleHolder) {
+                if (convertView != null && convertView.getTag() instanceof MsgHolder) {
+                    holder = (MsgHolder) convertView.getTag();
+                } else {
                     holder = new MsgHolder();
                     convertView = inflater.inflate(R.layout.msg, null);
                     //初始化状态元素
@@ -207,11 +224,10 @@ public class MainActivity extends Activity {
                     holder.content = (TextView) convertView.findViewById(R.id.msgContent);
                     holder.updateTime = (TextView) convertView.findViewById(R.id.msgTime);
                     holder.goodButton = (ImageButton) convertView.findViewById(R.id.msgGoodButton);
+                    holder.photoLayout = (TableLayout) convertView.findViewById(R.id.msgPhotoLayout);
                     convertView.setTag(holder);
-                } else {
-                    holder = (MsgHolder) convertView.getTag();
                 }
-                isTitleHolder = false;
+
                 //设置好友头像
                 holder.iconImg.setBackgroundResource((Integer) list.get(position - 1).get("icon"));
                 holder.iconImg.setOnClickListener(new View.OnClickListener() {
@@ -219,6 +235,7 @@ public class MainActivity extends Activity {
                     public void onClick(View v) {
                         Intent intent = new Intent(MainActivity.this, PhotoActivity.class);
                         startActivity(intent);
+                        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
                     }
                 });
                 //设置用户
@@ -249,8 +266,72 @@ public class MainActivity extends Activity {
                         }
                     }
                 });
+                //设置照片显示
+                if (list.get(position - 1).get("photos") != null) {
+                    List<Integer> photoList = (ArrayList<Integer>) list.get(position - 1).get("photos");
+                    TableRow row = null;
+                    for (int i = 0; i < photoList.size(); ++i) {
+                        if (i % 3 == 0) {
+                            row = new TableRow(MainActivity.this);
+                            holder.photoLayout.addView(row);
+                        }
+                        ImageView imageView = getImageView(photoList.get(i));
+                        row.addView(imageView);
+                    }
+
+
+//                    if (photoList.size() == 1) {
+//                        ImageView imageView = getImageView(photoList.get(0));
+////                        ViewGroup.MarginLayoutParams marginLayoutParams = new ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+////                        marginLayoutParams.setMargins(5, 5, 5, 5);
+////                        imageView.setLayoutParams(marginLayoutParams);
+//                        imageView.setAdjustViewBounds(true);
+//                        holder.photoLayout.addView(imageView);
+//                    }
+//                    } else if (photoList.size() <= 4) {
+//                        TableRow row = null;
+//                        for (int i = 0; i < photoList.size(); ++i) {
+//                            if (i % 2 == 0) {
+//                                row = new TableRow(MainActivity.this);
+//                                holder.photoLayout.addView(row);
+//                            }
+//                            ImageView imageView = getImageView(photoList.get(i));
+//                            row.addView(imageView);
+//                            ViewGroup.MarginLayoutParams marginLayoutParams = new ViewGroup.MarginLayoutParams(180, 180);
+//                            marginLayoutParams.setMargins(5, 5, 5, 5);
+//                            imageView.setLayoutParams(marginLayoutParams);
+//                            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//                        }
+//                    }
+//                    int size = (int) Math.ceil(Math.sqrt(photoList.size()));
+//                    TableRow row = null;
+//                    for (int i = 0; i < photoList.size(); ++i) {
+//                        if (i % size == 0) {
+//                            row = new TableRow(MainActivity.this);
+//                            holder.photoLayout.addView(row);
+//                            row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT));
+//                        }
+//                        ImageView imageView = new ImageView(MainActivity.this);
+//                        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+//                        imageView.setBackgroundResource(photoList.get(i));
+//                        imageView.setLayoutParams(new TableRow.LayoutParams(180, 135));
+//                        imageView.setPadding(5, 0, 5, 0);
+//                        row.addView(imageView);
+//                    }
+                    list.get(position - 1).put("photos", null);
+                }
                 return convertView;
             }
+        }
+
+        private ImageView getImageView(int id) {
+            ImageView imageView = new ImageView(MainActivity.this);
+            imageView.setBackgroundResource(id);
+            TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(150, 150);
+            layoutParams.setMargins(10, 10, 10, 10);
+            imageView.setLayoutParams(layoutParams);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            return imageView;
         }
     }
 }
